7c6a30dbd956ee064335c8cb5237c664d42ae291
Rene <rene.evertz.98@gmail.com>
2020-05-06 15:35:37 +0200

    Idee erfolgreich umgesetzt

---
+++ Clipboard/Clipboard/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clipboard
+{
+	class Program
+	{
+		[STAThread]
+		static void Main(string[] args)
+		{
+			try
+			{
+				System.Windows.Forms.Clipboard.SetText(args[0] ?? "Leider nicht geklappt. Anleitung genau gelesen?");
+			}
+			catch (Exception)
+			{
+				System.Windows.Forms.Clipboard.SetText("Leider nicht geklappt. Anleitung genau gelesen?");
+			}
+		}
+	}
+}
